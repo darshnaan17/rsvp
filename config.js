cca52d81d8569eb7c79661fe3a9e81bb92e70216
darshnaan17 <65986511+darshnaan17@users.noreply.github.com>
2026-08-16 15:54:37 -0400

    Connect RSVP submission endpoint

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.RSVP_CONFIG = {
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzBRbUfFbI9qW8UnPkzLi4sRyyA92w2e-eAqtrhvbE7MWGk_dPHxqqTOZYD3mvuXUxRCg/exec",
   deadline: "2026-10-01T00:00:00-04:00",
   hostName: "Jayesh Vala",
   hostPhone: "813-727-6708",
